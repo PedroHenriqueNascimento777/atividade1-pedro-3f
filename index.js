1e9575aed762d912500a5c6e0c639c127c08b644
PedrinXx777 <127054810+PedroHenriqueNascimento777@users.noreply.github.com>
2024-08-05 22:52:38 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const meuNome = "pedrin do 011";
+const idade = 18;
+const altura = "180cm";
+const massa = "62 Kg";
+const cidade = "SJP";
+
+
+console.log(meuNome)
+console.log(idade)
+console.log(altura)
+console.log(massa)
+console.log(cidade)
+// adicione nesse programa, altura, massa e cidade onde mora
